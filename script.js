4f27d53860d9284218b18839775c12d6c24f9da4
Enokine <enzo.rft@gmail.com>
2024-04-05 13:52:52 +0200

    Création du listener

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+const valeur = document.getElementById("input");
+var code_postal;
+
+input.addEventListener("input", updateValue);
+
+function updateValue(e) {
+  code_postal = e.target.value;
+}
+
 async function RequestMeteo(insee) {
     try {
         const res = await fetch(`https://api.meteo-concept.com/api/ephemeride/0?token=bed2667d06926b83c2780e247db4c70a28afc391d138efb8f2216f2de64bd7c0&insee=${insee}`);
