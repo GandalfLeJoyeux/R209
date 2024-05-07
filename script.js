2573e1bc6cdd4e84a518b99a6b57d5b0c4d811a6
Polidori Antoine <antoine.polidori2@gmail.com>
2024-05-07 15:34:39 +0200

    Modification Fonction RequestMeteo

--- script.js
+++ script.js
@@ -21,11 +21,12 @@ async function RequestMeteo(insee) {
     try {
         const res = await fetch(`https://api.meteo-concept.com/api/forecast/daily/0?token=bed2667d06926b83c2780e247db4c70a28afc391d138efb8f2216f2de64bd7c0&insee=${insee}`);
         const datares = await res.json();
-        return datares
+        return datares;
+        console.log(datares);
     }
 
     catch (error) {
-        console.error("Erreur lors de la requête API météo : ", error);
+        console.error("Il y a eu un problème : ", error);
         throw error;
     }
 }
